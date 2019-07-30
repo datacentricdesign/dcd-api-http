b188554c10e2c483f0043e4a72e2fe320fbf479e
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-07-30 16:45:39 +0200

    debug

--- routes/persons.js
+++ routes/persons.js
@@ -18,6 +18,120 @@ let auth = null;
 exports.setModelAndAuth = (newModel, newAuth) => {
   model = newModel;
   auth = newAuth;
+
+  /**
+   * @api {post} /persons Create
+   * @apiGroup Person
+   * @apiDescription Create a Person.
+   *
+   * @apiSuccess {object} personId Id of the created Person
+   */
+  router.post("/", (request, response) => {
+    const person = new Person(request.body);
+    model.persons
+      .create(person)
+      .then(result => success(response, { personId: result }))
+      .catch(error => fail(response, error));
+  });
+
+  /**
+   * @api {get} /persons List
+   * @apiGroup Person
+   * @apiDescription List Persons.
+   *
+   * @apiSuccess {array} persons Array of Persons found.
+   */
+  router.get(
+    "/",
+    auth.introspect,
+    auth.wardenSubject({ resource: "persons", action: "list" }),
+    (request, response) => {
+      model.persons
+        .list(request.user.sub)
+        .then(result => success(response, result))
+        .catch(error => fail(response, error));
+    }
+  );
+
+  /**
+   * @api {get} /persons/:personId Read
+   * @apiGroup Person
+   * @apiDescription Read a Person.
+   *
+   * @apiSuccess {object} person Person found.
+   */
+  router.get(
+    "/:entityId",
+    auth.introspect,
+    auth.wardenSubject({ resource: "persons", action: "read" }),
+    (request, response) => {
+      model.persons
+        .read(request.params.entityId)
+        .then(result => success(response, { person: result }))
+        .catch(error => fail(response, error));
+    }
+  );
+
+  /**
+   * @api {put} /persons/:personId Update
+   * @apiGroup Person
+   * @apiDescription Update a Person.
+   */
+  router.put(
+    "/:entityId",
+    auth.introspect,
+    auth.wardenSubject({ resource: "persons", action: "update" }),
+    (request, response) => {
+      const person = new Person(request.params.entityId, request.body);
+      model.persons
+        .update(person)
+        .then(result => success(response, result))
+        .catch(error => fail(response, error));
+    }
+  );
+
+  /**
+   * @api {delete} / Delete
+   * @apiGroup Person
+   * @apiDescription Delete a Person.
+   */
+  router.delete(
+    "/:entityId",
+    auth.introspect,
+    auth.wardenSubject({ resource: "persons", action: "delete" }),
+    (request, response) => {
+      const personId = request.params.entityId;
+      model.persons
+        .delete(personId)
+        .then(result => success(response, result))
+        .catch(error => fail(response, error));
+    }
+  );
+
+  /**
+   * @api {post} /persons/:personId/check Check
+   * @apiGroup Person
+   * @apiDescription Check a Person's credentials.
+   *
+   * @apiSuccess {object} person Person found.
+   */
+  router.post(
+    "/:entityId/check",
+    auth.introspect,
+    // auth.wardenToken({
+    //     resource: 'persons',
+    //     scope: ['dcd:auth'], action: 'check'
+    // }),
+    (request, response) => {
+      if (request.body !== undefined && request.body.password !== undefined) {
+        model.persons
+          .check(request.params.entityId, request.body.password)
+          .then(result => success(response, { person: result }))
+          .catch(error => fail(response, error));
+      }
+    }
+  );
+
   return Promise.resolve();
 };
 
@@ -30,116 +144,3 @@ const fail = (response, error) => {
   logger.error(error);
   utils.writeJson(response, error);
 };
-
-/**
- * @api {post} /persons Create
- * @apiGroup Person
- * @apiDescription Create a Person.
- *
- * @apiSuccess {object} personId Id of the created Person
- */
-router.post("/", (request, response) => {
-  const person = new Person(request.body);
-  model.persons
-    .create(person)
-    .then(result => success(response, { personId: result }))
-    .catch(error => fail(response, error));
-});
-
-/**
- * @api {get} /persons List
- * @apiGroup Person
- * @apiDescription List Persons.
- *
- * @apiSuccess {array} persons Array of Persons found.
- */
-router.get(
-  "/",
-  auth.introspect,
-  auth.wardenSubject({ resource: "persons", action: "list" }),
-  (request, response) => {
-    model.persons
-      .list(request.user.sub)
-      .then(result => success(response, result))
-      .catch(error => fail(response, error));
-  }
-);
-
-/**
- * @api {get} /persons/:personId Read
- * @apiGroup Person
- * @apiDescription Read a Person.
- *
- * @apiSuccess {object} person Person found.
- */
-router.get(
-  "/:entityId",
-  auth.introspect,
-  auth.wardenSubject({ resource: "persons", action: "read" }),
-  (request, response) => {
-    model.persons
-      .read(request.params.entityId)
-      .then(result => success(response, { person: result }))
-      .catch(error => fail(response, error));
-  }
-);
-
-/**
- * @api {put} /persons/:personId Update
- * @apiGroup Person
- * @apiDescription Update a Person.
- */
-router.put(
-  "/:entityId",
-  auth.introspect,
-  auth.wardenSubject({ resource: "persons", action: "update" }),
-  (request, response) => {
-    const person = new Person(request.params.entityId, request.body);
-    model.persons
-      .update(person)
-      .then(result => success(response, result))
-      .catch(error => fail(response, error));
-  }
-);
-
-/**
- * @api {delete} / Delete
- * @apiGroup Person
- * @apiDescription Delete a Person.
- */
-router.delete(
-  "/:entityId",
-  auth.introspect,
-  auth.wardenSubject({ resource: "persons", action: "delete" }),
-  (request, response) => {
-    const personId = request.params.entityId;
-    model.persons
-      .delete(personId)
-      .then(result => success(response, result))
-      .catch(error => fail(response, error));
-  }
-);
-
-/**
- * @api {post} /persons/:personId/check Check
- * @apiGroup Person
- * @apiDescription Check a Person's credentials.
- *
- * @apiSuccess {object} person Person found.
- */
-router.post(
-  "/:entityId/check",
-  auth.introspect,
-  // auth.wardenToken({
-  //     resource: 'persons',
-  //     scope: ['dcd:auth'], action: 'check'
-  // }),
-  (request, response) => {
-    if (request.body !== undefined && request.body.password !== undefined) {
-      model.persons
-        .check(request.params.entityId, request.body.password)
-        .then(result => success(response, { person: result }))
-        .catch(error => fail(response, error));
-    }
-  }
-);
