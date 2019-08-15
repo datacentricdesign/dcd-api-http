852c552d78b45bf2e132aa01600955f0282b7138
issarompion <irompion@yahoo.fr>
2019-08-15 09:19:31 +0200

    create /stat/PropertyTypes route

--- routes/stats.js
+++ routes/stats.js
@@ -57,6 +57,49 @@ this.router.get("/:propertyType",
     }
   );
 
+/**
+* @api {get} /stats/propertyTypes Property Types
+* @apiGroup Stat
+* @apiDescription Get number of properties, entities and values of a property type in a range date of an array of properties. There must be values for the property entities to be counted in the range.
+*
+* @apiHeader {String} Authorization TOKEN ID
+
+* @apiParam (Body) {Interaction} interaction Interaction to create as JSON.
+
+* @apiParamExample {json} array of property types:
+*     {
+*       "propertyTypes": ["LOCATION","THREE_DIMENSIONS"],
+*     }
+* 
+* @apiParam {Number} [from] Optional Query timestamp from of the range date.
+* @apiParam {Number} [to] Optional Query timestamp to of the range date.
+*
+* @apiSuccess {json} Json of a property type stats.
+*/
+this.router.get("/propertyTypes",
+    this.auth.introspect,
+    (request, response) => {
+      if (
+        request.body === undefined ||
+        request.body.propertyTypes === undefined ||
+        !Array.isArray(request.body.propertyTypes)
+      ) {
+        return this.fail(
+          new Error(
+            "Request body is undefined" +
+              " or request body property types is undefined" +
+              "or request body property types is not an array"
+          )
+        );
+      }
+
+      this.model.stats
+        .getTypesStats(propertyTypes, from, to)
+        .then(result => this.success(response, { stat: result }))
+        .catch(error => this.fail(response, error));
+    }
+  );
+
   }
 }
 
