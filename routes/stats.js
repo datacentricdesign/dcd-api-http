ce0b6af4f6f5345997ec8a3777627f39bc180455
issarompion <irompion@yahoo.fr>
2019-08-15 14:02:01 +0200

    creation of a single route for stats type

--- routes/stats.js
+++ routes/stats.js
@@ -63,37 +63,25 @@ this.router.get("/propertyTypes/:propertyType",
 * @apiDescription Get number of properties, entities and values of a property type in a range date of an array of properties. There must be values for the property entities to be counted in the range.
 *
 * @apiHeader {String} Authorization TOKEN ID
-
-* @apiParam (Body) {Interaction} interaction Interaction to create as JSON.
-
-* @apiParamExample {json} array of property types:
-*     {
-*       "propertyTypes": ["LOCATION","THREE_DIMENSIONS"],
-*     }
-* 
-* @apiParam {Number} [from] Optional Query timestamp from of the range date.
-* @apiParam {Number} [to] Optional Query timestamp to of the range date.
-*
+* @apiParam (Query) {Array} [types] array of property type.
+* @apiParam (Query) {Number} [from] Optional timestamp from of the range date.
+* @apiParam (Query) {Number} [to] Optional Query timestamp to of the range date.
+* @apiParamExample {String} query:
+* "https://dwd.tudelft.nl/api/stats/propertyTypes?types=LOCATION,THREE_DIMENSIONS&from=0&to=1565870227438"
 * @apiSuccess {json} Json of a property type stats.
 */
 this.router.get("/propertyTypes",
     this.auth.introspect,
     (request, response) => {
       if (
-        request.body === undefined ||
-        request.body.propertyTypes === undefined ||
-        !Array.isArray(request.body.propertyTypes)
-      ) {
+        request.query.types === undefined 
+        ) {
         return this.fail(
-          new Error(
-            "Request body is undefined" +
-              " or request body property types is undefined" +
-              "or request body property types is not an array"
-          )
+          new Error("types is undefined")
         );
       }
 
-      let propertyTypes = request.body.propertyTypes
+      let propertyTypes = request.query.types.split(',')
       let from;
       let to;
       if (request.query.from !== undefined) {
@@ -107,7 +95,7 @@ this.router.get("/propertyTypes",
         .getTypesStats(propertyTypes, from, to)
         .then(result => this.success(response, { stats: result }))
         .catch(error => this.fail(response, error));
-    }
+      }
   );
 
   }
