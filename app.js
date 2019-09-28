6aa4b8f742c5bdac520aef1897609ad34fea4c1d
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-09-28 19:54:19 +0200

    stringify error

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use(function(request, response, next) {
 // eslint-disable-next-line no-unused-vars
 app.use((error, request, response, next) => {
   if (error instanceof DCDError) {
-    logger.error(error);
+    logger.error(JSON.stringify(error));
   } else {
     logger.error(JSON.stringify(error, Object.getOwnPropertyNames(error)));
   }
